ce35f438bf930e44102ede63f36555d983a164be
ravensorrow <xaero@xaerolimit.net>
2013-03-04 02:40:12 -0500

    Hello World

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+# Non-Programmer's Tutorial for Python 2.6/Hello, World
+# URL: http://en.wikibooks.org/wiki/Non-Programmer%27s_Tutorial_for_Python_2.6/Hello,_World
+
+__author__ = 'chris'
+
+print "Hello, World!"
