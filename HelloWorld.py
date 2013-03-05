5831381355ac1357d370a65f3f2fa663bdc001c0
ravensorrow <xaero@xaerolimit.net>
2013-03-05 10:15:20 -0500

    Updated header for env Added version number to comments

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python2
+
 # Non-Programmer's Tutorial for Python 2.6/Hello, World
 # URL: http://en.wikibooks.org/wiki/Non-Programmer%27s_Tutorial_for_Python_2.6/Hello,_World
+
+# Version 0.2
 
 __author__ = 'chris'
 
