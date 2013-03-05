f143133cd653cd8d5a20c5c85f822eac5a9cb767
ravensorrow <xaero@xaerolimit.net>
2013-03-05 10:19:46 -0500

    Created new file (Expressions.py) Added env header for execution Added to localgit repo for commit to GitHub

---
+++ Expressions.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+
+# Non-Programmer's Tutorial for Python 2.6/Hello, World
+# http://en.wikibooks.org/wiki/Non-Programmer%27s_Tutorial_for_Python_2.6/Hello,_World#Expressions
+__author__ = 'chris'
+
+print "2 + 2 is", 2 + 2
+print "3 * 4 is", 3 * 4
+print "100 - 1 is", 100 - 1
+print "(33 + 2) / 5 + 11.5 is", (33 + 2) / 5 + 11.5
+
+print
+
+print "... ... ..."
+
+print
+
+print "     Operation           Symbol      Example"
+print "Power (exponentiation) 	** 	        5 ** 2 == 25"
+print "Multiplication 	        * 	        2 * 3 == 6"
+print "Division 	            / 	        14 / 3 == 4"
+print "Remainder (modulo) 	    % 	        14 % 3 == 2"
+print "Addition 	            + 	        1 + 2 == 3"
+print "Subtraction             - 	        4 - 3 == 1"
+
+print
+
+print "... ... ..."
+
+print
+
+print "14 / 3 = ", 14 / 3
+print "14 % 3 = ", 14 % 3
+print
+print "14.0 / 3.0 =", 14.0 / 3.0
+print "14.0 % 3.0 =", 14.0 % 3.0
+print
+print "14.0 / 3 =", 14.0 / 3
+print "14.0 % 3 =", 14.0 % 3
+print
+print "14 / 3.0 =", 14 / 3.0
+print "14 % 3.0 =", 14 % 3.0
+
+print
+
+print "... ... ..."
+
+print
+
+# Not quite PI, but an incredible simulation.
+print 22.0 / 7.0    # 355/113 is even more incredible rational approx to PI.
+print 355 / 113.0     # I (Chris) had to add .0 to get it to print the decimal.
