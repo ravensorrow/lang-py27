8c4b613a6847d4f98216c39e3261443b129f6c48
ravensorrow <xaero@xaerolimit.net>
2013-03-08 15:23:49 -0500

    Updated version numbers, added new file

---
+++ Exercise_02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+
+# Non-Programmer's Tutorial for Python 2.6/Hello, World
+# http://en.wikibooks.org/wiki/Non-Programmer%27s_Tutorial_for_Python_2.6/Hello,_World#Exercises
+
+__author__ = 'chris'
+__version__ = '0.1'
+
